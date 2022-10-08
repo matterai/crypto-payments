ed01f2440cefd36445a989e79843a3c275abdcb7
matterai <matterai.net@gmail.com>
2022-10-08 20:53:58 -0300

    Initial react-app commit

---
+++ src/src/App.tsx
@@ -0,0 +1,16 @@
+import { Component } from 'react';
+import './App.css';
+import { Container } from '@mui/material';
+
+
+class App extends Component {
+  render() {
+    return(
+      <Container maxWidth="sm">
+        Test
+      </Container>
+    );
+  }
+}
+
+export default App;
