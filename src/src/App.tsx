c46970ccad58c052e40c1e4f27ed6b9d0d8aacf2
matterai <matterai.net@gmail.com>
2022-10-09 11:06:33 -0300

    Header refactor.

--- src/src/App.tsx
+++ src/src/App.tsx
@@ -1,14 +1,14 @@
 import { Component } from 'react';
 import './App.css';
 import { Container } from '@mui/material';
-import Header from './components/Header';
+import { Header } from './components/Header';
 
 
 class App extends Component {
   render() {
     return(
       <Container maxWidth="sm">
-        <Header />
+        <Header user={null} />
       </Container>
     );
   }
--- src/src/components/Header.tsx
+++ src/src/components/Header.tsx
@@ -1,30 +1,28 @@
 import { AppBar, Box, Button, IconButton, Toolbar, Typography } from "@mui/material";
-import { Component } from "react";
 import MenuIcon from '@mui/icons-material/Menu';
 
-class Header extends Component {
-  render() {
-    return (
-      <Box sx={{ flexGrow: 1 }}>
-        <AppBar position="static">
-          <Toolbar>
-            <IconButton
-              size="large"
-              edge="start"
-              color="inherit"
-              aria-label="menu"
-              sx={{ mr: 2 }}>
-                <MenuIcon />
-            </IconButton>
-            <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
-              Crypto Payment Demo
-            </Typography>
-            <Button color="inherit">Authorize</Button>
-          </Toolbar>
-        </AppBar>
-      </Box>
-    );
-  }
+type HeaderProps = {
+  user: UserProps | null;
 }
 
-export default Header;
+type UserProps = {
+  id: string;
+  evmAddress: string;
+}
+
+export const Header = (props: HeaderProps) => {
+  const render = (
+    <Box sx={{ flexGrow: 1 }}>
+      <AppBar position="static">
+        <Toolbar>
+          <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
+            Crypto Market
+          </Typography>
+          <Button color="inherit">Login</Button>
+        </Toolbar>
+      </AppBar>
+    </Box>
+  );
+
+  return render;
+}
