671a4ac81b4fce7865174d68c3035e4eb8f457fe
matterai <matterai.net@gmail.com>
2022-10-10 19:57:36 -0300

    Metamask consistentcy for warning

--- client/src/components/Announcements/index.tsx
+++ client/src/components/Announcements/index.tsx
@@ -3,19 +3,36 @@ import { useEffect, useState } from "react";
 import { isMetamaskInstalled } from "../../utils/Metamask";
 
 export const Announcements = () => {
-  const [chainId, setChainId] = useState<number>(window.ethereum ? Number(window.ethereum.chainId) : 0);
+  const [chainId, setChainId] = useState<number>(0);
 
   const onChainChangedHandler = (chainId: number) => {
     setChainId(chainId);
   }
 
+  const onPolygonLinkClick = () => {
+    window.ethereum?.request({
+      method: 'wallet_switchEthereumChain',
+      params: [{ chainId: "0x89" }]
+    });
+    setChainId(137);
+  }
+
   useEffect(() => {
+    async function fetchChainId() {
+      if (window.ethereum) {
+        const chainId = Number(await window.ethereum.request({ method: "eth_chainId" }));
+        onChainChangedHandler(chainId);
+      }
+    };
+
     if (window.ethereum) {
+      fetchChainId();
+      
       window.ethereum.on("chainChanged", (chainId) => {
         return onChainChangedHandler(Number(chainId));
       });
     }
-  });
+  }, []);
 
   return (
     <Box component="div" sx={{ p: 1 }}>
@@ -26,7 +43,7 @@ export const Announcements = () => {
       }
       { 
         isMetamaskInstalled() && chainId !== 137 
-          ? <Alert severity="warning">Please, switch your wallet to Polygon.</Alert> 
+          ? <Alert severity="warning">Please, switch your wallet to <Link href="#" onClick={onPolygonLinkClick}>Polygon</Link>.</Alert> 
           : null 
       }
     </Box>
