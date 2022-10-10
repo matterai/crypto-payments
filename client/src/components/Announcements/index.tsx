734103307f3220484681880a3c46fb16e943dea4
matterai <matterai.net@gmail.com>
2022-10-10 19:22:56 -0300

    Fixed warning if chainId is wrong.

--- client/src/components/Announcements/index.tsx
+++ client/src/components/Announcements/index.tsx
@@ -1,11 +1,34 @@
 import { Alert, Box, Link } from "@mui/material";
+import { useEffect, useState } from "react";
 import { isMetamaskInstalled } from "../../utils/Metamask";
 
 export const Announcements = () => {
-  return !isMetamaskInstalled() 
-    ? 
-      <Box component="div" sx={{ p: 1 }}>
-        <Alert severity="warning">Please, install <Link href="https://metamask.io">MetaMask</Link> extension for your browser to login.</Alert>
-      </Box>
-    : null;
+  const [chainId, setChainId] = useState<number>(window.ethereum ? Number(window.ethereum.chainId) : 0);
+
+  const onChainChangedHandler = (chainId: number) => {
+    setChainId(chainId);
+  }
+
+  useEffect(() => {
+    if (window.ethereum) {
+      window.ethereum.on("chainChanged", (chainId) => {
+        return onChainChangedHandler(Number(chainId));
+      });
+    }
+  });
+
+  return (
+    <Box component="div" sx={{ p: 1 }}>
+      { 
+        !isMetamaskInstalled() 
+          ? <Alert severity="warning">Please, install <Link href="https://metamask.io">MetaMask</Link> extension for your browser to login.</Alert> 
+          : null 
+      }
+      { 
+        isMetamaskInstalled() && chainId !== 137 
+          ? <Alert severity="warning">Please, switch your wallet to Polygon.</Alert> 
+          : null 
+      }
+    </Box>
+  );
 }
